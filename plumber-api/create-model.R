2537ff0d3999c874c1c6155c3b446eedb2a6d2b5
Alison Clarke <alison.r.clarke@durham.ac.uk>
2020-04-09 16:16:24 +0100

    Remove plumber-api folder as we're not using it

--- plumber-api/create-model.R
+++
@@ -1,8 +0,0 @@
-library(ggplot2) ## for dataset
-library(readr) ## for write_rds
-data(diamonds)
-
-model <- lm(price ~ carat, data = diamonds)
-
-write_rds(model, "lm-model.rds")
-
--- plumber-api/score-lm.R
+++
@@ -1,9 +0,0 @@
-# read model
-model <- readRDS("lm-model.rds")
-
-#* Score using linear model
-#* @param carat The carat value of the diamond
-#* @get /score-lm
-function(carat) {
-  list(price = predict(model, newdata = data.frame(carat = as.numeric(carat))))
-}
